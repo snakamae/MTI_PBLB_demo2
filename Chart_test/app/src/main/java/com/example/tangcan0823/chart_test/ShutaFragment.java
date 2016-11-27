8256b5b4584f14fe9a1416e1e0a017a60ccdf084
snakamae <nakamae@iplab.cs.tsukuba.ac.jp>
2016-11-27 19:15:02 +0900

    It worksgit add .!

--- Chart_test/app/src/main/java/com/example/tangcan0823/chart_test/ShutaFragment.java
+++ Chart_test/app/src/main/java/com/example/tangcan0823/chart_test/ShutaFragment.java
@@ -22,7 +22,7 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
-    
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
